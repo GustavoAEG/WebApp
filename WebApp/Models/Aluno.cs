0af1b67af89f3c1db73a0155cd051234d13f792e
gustavo.goncalves <gustavo.goncalves@FSMAXBDEV-08>
2023-04-04 17:40:37 -0300

    Add project files.

---
+++ WebApp/Models/Aluno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class Aluno
+    {
+        public string Nome { get; set; }
+
+        public int Idade { get; set; }
+
+
+    }
+}
